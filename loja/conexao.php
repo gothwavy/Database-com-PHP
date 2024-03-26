65ac555dbc3586dfa097518fa5b4f261d628e67a
Ryan Almeida <113449303+gothwavy@users.noreply.github.com>
2024-03-25 22:22:08 -0300

    Delete loja directory

--- loja/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost"; // padrão - Loca
-$database   = "loja"; 
-$username   = "root";
-$password   = "";
-// Criar conexão -> Padrão para qualquer conexão
-$conexao= mysqli_connect($servername, $username, $password, $database);
-?>
